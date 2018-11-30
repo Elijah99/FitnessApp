1ab1c1ef73aab756b191edbf6597e7e9679d2605
SergeiBY <mikhailovskii.s@tut.by>
2018-11-30 22:26:14 +0300

    Added adding activity

--- app/src/main/java/asus/example/com/fitnessapp/AddingProgramOrArticleActivity.java
+++ app/src/main/java/asus/example/com/fitnessapp/AddingProgramOrArticleActivity.java
@@ -2,6 +2,10 @@ package asus.example.com.fitnessapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import java.util.Objects;
 
 public class AddingProgramOrArticleActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class AddingProgramOrArticleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adding_program_or_article);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        switch (menuItem.getItemId()){
+            case R.id.home:
+                this.finish();
+                return true;
+            default:
+                    return onOptionsItemSelected(menuItem);
+        }
     }
 }
