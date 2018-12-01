c5f89e647d165991daf807ccddc4a8d38d0dd931
SergeiBY <mikhailovskii.s@tut.by>
2018-12-01 21:50:19 +0300

    Added Save button and fixed Up button

--- app/src/main/java/asus/example/com/fitnessapp/AddingProgramOrArticleActivity.java
+++ app/src/main/java/asus/example/com/fitnessapp/AddingProgramOrArticleActivity.java
@@ -3,7 +3,9 @@ package asus.example.com.fitnessapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.Objects;
 
@@ -15,18 +17,28 @@ public class AddingProgramOrArticleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_adding_program_or_article);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem menuItem){
-        switch (menuItem.getItemId()){
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
             case R.id.home:
                 this.finish();
                 return true;
+            case R.id.save:
+                Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show();
+                return true;
             default:
-                    return onOptionsItemSelected(menuItem);
+                return super.onOptionsItemSelected(item);
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.save, menu);
+        return true;
+    }
+
 }
